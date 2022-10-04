edb18c6a9d12832982fc470fc71a028965aadd5b
Chris Landtiser <chris@landtiser.com>
2022-10-04 14:31:35 -0700

    First functional commit
    First commit with functional templates and blog connection to Storyblok. Further templating and styling needed.

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import storyblok from "@storyblok/astro";
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://landtiser.com',
+	integrations: [
+		mdx(),
+		sitemap(),
+		storyblok({
+			accessToken: "ZtzlWPFXkDLC0DoS5gpisgtt",
+			apiOptions: {
+				region: 'us'
+			},
+			components: {
+				page: 'storyblok/Page',
+				feature: 'storyblok/Feature',
+				grid: 'storyblok/Grid',
+				teaser: 'storyblok/Teaser',
+				blogpost: 'storyblok/Blogpost'
+			}
+		  }),
+	],
+	
+});
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Chris Landtiser | Designer & Developer';
+export const SITE_DESCRIPTION = 'I work with clients of all sizes, from small businesses through enterprise teams, to design and develop modern web solutions that turn goals into reality.';
